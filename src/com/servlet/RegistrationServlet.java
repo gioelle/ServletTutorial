12761389931da53b175761a8a550415181f00d70
gioelle <fronsaglia@gmail.com>
2017-06-29 13:28:31 -0500

    Servlet tutorial initial add

---
+++ src/com/servlet/RegistrationServlet.java
@@ -0,0 +1,59 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.user.Address;
+import com.user.Student;
+
+
+@WebServlet("/RegistrationServlet")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public RegistrationServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Student student = new Student();
+		student.setEmail(request.getParameter("email"));
+		student.setFirstName(request.getParameter("firstName"));
+		student.setLastName(request.getParameter("lastName"));
+		student.setPassword(request.getParameter("password"));
+		student.setSchoolName("Claim Academy");
+		Address a = new Address();
+		student.getAddress().setStreet(request.getParameter("street"));
+		student.getAddress().setCity(request.getParameter("city"));
+		student.getAddress().setState(request.getParameter("state"));
+		student.getAddress().setZipCode(request.getParameter("zipCode"));
+		student.setAddress(a);
+		
+		HttpSession session = request.getSession(true);
+		session.setAttribute("student", student);
+		
+		RequestDispatcher rs = request.getRequestDispatcher("account.jsp");
+		rs.forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+}
